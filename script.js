2825d085b556fc056fb0b3d49f184407f83ecf0b
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-23 12:49:30 +0400

    'nato'

---
+++ script.js
@@ -0,0 +1,23 @@
+input.onchange = function(){
+    let words = this.value;
+    let result = '';
+    let nato = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"];
+    let regExp = /\s+/g;
+    let natoWord = words.replace(regExp, '');
+    natoWord = natoWord.toLowerCase().split('');
+    for(let i=0; i<natoWord.length; i++){
+        for(let j=0; j<nato.length; j++){
+            if(natoWord[i].toUpperCase() === nato[j][0] ){
+                result = result + nato[j] + ' ';
+            }
+
+        }
+        if(natoWord[i] == '!' | natoWord[i] == '?' | natoWord[i] == ','| natoWord[i] == '.'){
+            result = result+ natoWord[i]+ ' ';
+        }
+    }
+    // let arr = result.split(" ");
+    // arr.pop();
+    // result = arr.join(" ");
+    NATO.innerHTML = result;
+}
