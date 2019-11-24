ae3f1b83506c3e2a3977aa4fade726a566749c3c
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-24 21:32:26 +0400

    optimization NATO

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-input.onchange = function(){
-    let words = this.value;
-    let result = '';
-    let nato = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"];
-    let regExp = /\s+/g;
-    let natoWord = words.replace(regExp, '');
-    natoWord = natoWord.toLowerCase().split('');
-    for(let i=0; i<natoWord.length; i++){
-        for(let j=0; j<nato.length; j++){
-            if(natoWord[i].toUpperCase() === nato[j][0] ){
-                result = result + nato[j] + ' ';
-            }
-
-        }
-        if(natoWord[i] == '!' | natoWord[i] == '?' | natoWord[i] == ','| natoWord[i] == '.'){
-            result = result+ natoWord[i]+ ' ';
-        }
-    }
-    NATO.innerHTML = result;
+function nato(str){
+    const nato = {
+        'a':"Alfa", 
+        'b':"Bravo", 
+        'c':"Charlie", 
+        'd':"Delta",
+        'e':"Echo", 
+        'f':"Foxtrot",
+        'g':"Golf",
+        'h':"Hotel",
+        'i':"India",
+        'j':"Juliett",
+        'k':"Kilo",
+        'l':"Lima",
+        'm':"Mike", 
+        'n':"November", 
+        'o':"Oscar",
+        'p':"Papa",
+        'q':"Quebec",
+        'r':"Romeo",
+        's':"Sierra",
+        't':"Tango",
+        'u':"Uniform",
+        'v':"Victor",
+        'w':"Whiskey",
+        'x':"Xray",
+        'y':"Yankee",
+        'z':"Zulu",
+        '?':'?',
+        '.':".",
+        '!':"!"
+    };
+    let res = '';
+    for(let value of str){
+        if(value === ' ') continue;
+        value = value.toLowerCase();
+        res = res + nato[value] + ' ';
+    };
+    return res.trim();
 }
+
+input.onchange = function(){
+    NATO.innerHTML = nato(this.value);
+};
