f4f3ba142076e23349616cb2fa65cd19adcbb561
Stanislav Vlasov <vvs00794@gmail.com>
2019-11-24 22:07:24 +0400

    NATO3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function nato(str){
     const nato = {
         'a':"Alfa", 
@@ -38,28 +37,8 @@ function nato(str){
         res = res + nato[value] + ' ';
     };
     return res.trim();
-=======
-input.onchange = function(){
-    let words = this.value;
-    let result = '';
-    let nato = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"];
-    let regExp = /\s+/g;
-    let natoWord = words.replace(regExp, '');
-    natoWord = natoWord.toLowerCase().split('');
-    for(let i=0; i<natoWord.length; i++){
-        for(let j=0; j<nato.length; j++){
-            if(natoWord[i].toUpperCase() === nato[j][0] ){
-                result = result + nato[j] + ' ';
-            }
+};
 
-        }
-        if(natoWord[i] == '!' | natoWord[i] == '?' | natoWord[i] == ','| natoWord[i] == '.'){
-            result = result+ natoWord[i]+ ' ';
-        }
-    }
-    NATO.innerHTML = result;
->>>>>>> 4979bf406a943e6472e022bca7fb3938c634dea5
-}
 
 input.onchange = function(){
     NATO.innerHTML = nato(this.value);
